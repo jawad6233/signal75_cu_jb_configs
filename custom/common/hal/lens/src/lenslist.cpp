0314a5963d96ae52226b84824b44e48d81b93ed8
IICuX <iicux@Fucking-laptop.(none)>
2014-09-04 14:18:44 +0300

    first commit

---
+++ custom/common/hal/lens/src/lenslist.cpp
@@ -0,0 +1,146 @@
+/* Copyright Statement:
+ *
+ * This software/firmware and related documentation ("MediaTek Software") are
+ * protected under relevant copyright laws. The information contained herein
+ * is confidential and proprietary to MediaTek Inc. and/or its licensors.
+ * Without the prior written permission of MediaTek inc. and/or its licensors,
+ * any reproduction, modification, use or disclosure of MediaTek Software,
+ * and information contained herein, in whole or in part, shall be strictly prohibited.
+ */
+/* MediaTek Inc. (C) 2010. All rights reserved.
+ *
+ * BY OPENING THIS FILE, RECEIVER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+ * THAT THE SOFTWARE/FIRMWARE AND ITS DOCUMENTATIONS ("MEDIATEK SOFTWARE")
+ * RECEIVED FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE PROVIDED TO RECEIVER ON
+ * AN "AS-IS" BASIS ONLY. MEDIATEK EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NONINFRINGEMENT.
+ * NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY WHATSOEVER WITH RESPECT TO THE
+ * SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY, INCORPORATED IN, OR
+ * SUPPLIED WITH THE MEDIATEK SOFTWARE, AND RECEIVER AGREES TO LOOK ONLY TO SUCH
+ * THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO. RECEIVER EXPRESSLY ACKNOWLEDGES
+ * THAT IT IS RECEIVER'S SOLE RESPONSIBILITY TO OBTAIN FROM ANY THIRD PARTY ALL PROPER LICENSES
+ * CONTAINED IN MEDIATEK SOFTWARE. MEDIATEK SHALL ALSO NOT BE RESPONSIBLE FOR ANY MEDIATEK
+ * SOFTWARE RELEASES MADE TO RECEIVER'S SPECIFICATION OR TO CONFORM TO A PARTICULAR
+ * STANDARD OR OPEN FORUM. RECEIVER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S ENTIRE AND
+ * CUMULATIVE LIABILITY WITH RESPECT TO THE MEDIATEK SOFTWARE RELEASED HEREUNDER WILL BE,
+ * AT MEDIATEK'S OPTION, TO REVISE OR REPLACE THE MEDIATEK SOFTWARE AT ISSUE,
+ * OR REFUND ANY SOFTWARE LICENSE FEES OR SERVICE CHARGE PAID BY RECEIVER TO
+ * MEDIATEK FOR SUCH MEDIATEK SOFTWARE AT ISSUE.
+ *
+ * The following software/firmware and/or related documentation ("MediaTek Software")
+ * have been modified by MediaTek Inc. All revisions are subject to any receiver's
+ * applicable license agreements with MediaTek Inc.
+ */
+
+/********************************************************************************************
+ *     LEGAL DISCLAIMER
+ *
+ *     (Header of MediaTek Software/Firmware Release or Documentation)
+ *
+ *     BY OPENING OR USING THIS FILE, BUYER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+ *     THAT THE SOFTWARE/FIRMWARE AND ITS DOCUMENTATIONS ("MEDIATEK SOFTWARE") RECEIVED
+ *     FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE PROVIDED TO BUYER ON AN "AS-IS" BASIS
+ *     ONLY. MEDIATEK EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES, EXPRESS OR IMPLIED,
+ *     INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ *     A PARTICULAR PURPOSE OR NONINFRINGEMENT. NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY
+ *     WHATSOEVER WITH RESPECT TO THE SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY,
+ *     INCORPORATED IN, OR SUPPLIED WITH THE MEDIATEK SOFTWARE, AND BUYER AGREES TO LOOK
+ *     ONLY TO SUCH THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO. MEDIATEK SHALL ALSO
+ *     NOT BE RESPONSIBLE FOR ANY MEDIATEK SOFTWARE RELEASES MADE TO BUYER'S SPECIFICATION
+ *     OR TO CONFORM TO A PARTICULAR STANDARD OR OPEN FORUM.
+ *
+ *     BUYER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S ENTIRE AND CUMULATIVE LIABILITY WITH
+ *     RESPECT TO THE MEDIATEK SOFTWARE RELEASED HEREUNDER WILL BE, AT MEDIATEK'S OPTION,
+TO REVISE OR REPLACE THE MEDIATEK SOFTWARE AT ISSUE, OR REFUND ANY SOFTWARE LICENSE
+ *     FEES OR SERVICE CHARGE PAID BY BUYER TO MEDIATEK FOR SUCH MEDIATEK SOFTWARE AT ISSUE.
+ *
+ *     THE TRANSACTION CONTEMPLATED HEREUNDER SHALL BE CONSTRUED IN ACCORDANCE WITH THE LAWS
+ *     OF THE STATE OF CALIFORNIA, USA, EXCLUDING ITS CONFLICT OF LAWS PRINCIPLES.
+ ************************************************************************************************/
+
+#include <utils/Log.h>
+#include <utils/Errors.h>
+#include <fcntl.h>
+#include <math.h>
+
+#include "MediaHal.h"
+
+//#include "lens_custom_cfg.h"
+//#include "msdk_lens_exp.h"
+#include "camera_custom_lens.h"
+//#include "lens.h"
+//nclude "image_sensor.h"
+#include "kd_imgsensor.h"
+
+extern PFUNC_GETLENSDEFAULT pDummy_getDefaultData;
+
+#if defined(SENSORDRIVE)
+extern PFUNC_GETLENSDEFAULT pSensorDrive_getDefaultData;
+#endif
+
+#if defined(FM50AF)
+extern PFUNC_GETLENSDEFAULT pFM50AF_getDefaultData;
+#endif
+
+#if defined(MT9P017AF)
+extern PFUNC_GETLENSDEFAULT pMT9P017AF_getDefaultData;
+#endif
+//<2012/09/21-14219-alberthsiao,update camera driver for Hawk40
+#if defined(OV8835AF)
+extern PFUNC_GETLENSDEFAULT pOV8835AF_getDefaultData;
+//<2013/01/23-ClarkLin-add 2nd source of AF driver
+extern PFUNC_GETLENSDEFAULT pOV8835AF_getDefaultData_LiteON;
+//>2013/01/23-ClarkLin
+#endif
+
+MSDK_LENS_INIT_FUNCTION_STRUCT LensList[MAX_NUM_OF_SUPPORT_LENS] =
+{
+	{DUMMY_SENSOR_ID, DUMMY_LENS_ID, "Dummy", pDummy_getDefaultData},
+
+#if defined(SENSORDRIVE)
+	{DUMMY_SENSOR_ID, SENSOR_DRIVE_LENS_ID, "kd_camera_hw", pSensorDrive_getDefaultData},	
+
+    //  for backup lens, need assign correct SensorID
+    //{OV5642_SENSOR_ID, SENSOR_DRIVE_LENS_ID, "kd_camera_hw", pSensorDrive_getDefaultData},
+#endif
+
+#if defined(OV8835AF)
+//<2012/10/30 alberthsiao-15959, Add OV8835_mipi_raw  camera driver for hawk40
+//<2013/01/07-ClarkLin-[Hawk4.0]Add Camera second source
+//<2013/01/29-ClarkLin-Adjust camera order
+//	{OV8835MIPI_SEMCO_GENIUS_SENSOR_ID, OV8835AF_LENS_ID, "OV8835AF", pOV8835AF_getDefaultData},
+//>2013/01/29-ClarkLin
+	{OV8835MIPI_SEMCO_KANTATSU_SENSOR_ID, OV8835AF_LENS_ID, "OV8835AF", pOV8835AF_getDefaultData},
+//<2013/01/23-ClarkLin-add 2nd source of AF driver
+	{OV8835MIPI_LITEON_GENIUS_SENSOR_ID, OV8835AF_LENS_ID_LITEON, "OV8835AF", pOV8835AF_getDefaultData_LiteON},
+//>2013/01/23-ClarkLin
+//<2013/01/29-ClarkLin-Adjust camera order
+	{OV8835MIPI_SEMCO_GENIUS_SENSOR_ID, OV8835AF_LENS_ID, "OV8835AF", pOV8835AF_getDefaultData},
+//>2013/01/29-ClarkLin
+//>2013/01/07-ClarkLin
+//>2012/10/30 alberthsiao-15959
+#endif
+//>2012/09/21-14219-alberthsiao
+#if defined(FM50AF)
+	{DUMMY_SENSOR_ID, FM50AF_LENS_ID, "FM50AF", pFM50AF_getDefaultData},
+#endif
+
+    //  for new added lens, need assign correct SensorID
+#if defined(MT9P017AF)
+	{MT9P017MIPI_SENSOR_ID, MT9P017AF_LENS_ID, "MT9P017AF", pMT9P017AF_getDefaultData},
+#endif
+
+};
+
+UINT32 GetLensInitFuncList(PMSDK_LENS_INIT_FUNCTION_STRUCT pLensList)
+{
+    memcpy(pLensList, &LensList[0], sizeof(MSDK_LENS_INIT_FUNCTION_STRUCT)* MAX_NUM_OF_SUPPORT_LENS);
+    return MHAL_NO_ERROR;
+} // GetLensInitFuncList()
+
+
+
+
+
+
---
+++ custom/common/kernel/lcm/mt65xx_lcm_list.c
@@ -0,0 +1,481 @@
+#include <lcm_drv.h>
+extern LCM_DRIVER otm8009a_dsi_lcm_drv;
+extern LCM_DRIVER lt5002f_dsi_lcm_drv;
+extern LCM_DRIVER gt397_otm8009a_lcm_drv;
+//!!!!!!
+extern LCM_DRIVER otm8009a_fwvga_dsi_cmd_tianma_lcm_drv;
+extern LCM_DRIVER otm8009a_fwvga_dsi_vdo_tianma_lcm_drv;
+extern LCM_DRIVER otm8009a_dsi_lcm_drv;
+extern LCM_DRIVER otm8009_dsi_6517_lcm_drv;
+extern LCM_DRIVER otm8009a_lcm_drv;
+extern LCM_DRIVER hx8369a_wvga_dsi_cmd_drv;
+extern LCM_DRIVER hx8369a_wvga_dsi_vdo_drv;
+extern LCM_DRIVER otm8009a_hansen_hs4534fwnc23_00_dsi_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_huaxian_HX451023FP_BOE_dsi_2_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_haifei_HF_14539_2059_A0_CMI_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_hengsheng_HS045IK_01B_TM_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_nisin_NST446FW2302ANK_TM_dsi_2_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_hengsheng_HS040IH_01E_TM_mcu18_wvga_lcm_drv;
+extern LCM_DRIVER hx8369a_HUARUI_MS_HX450939FP_BOE_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER hx8369a_lcm_drv;
+extern LCM_DRIVER otm8009a_sifang_mcu_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_aizhuoertai_a4019n50q0_mcu_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hongsheng_hs4501071a_dsi_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hongtao_HT45009A_V0_CPT_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_nisin_NSNST397WV3923ANP_CMI_mcu24_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hongda_HDKM0493_CMI_dsi_2_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hentongkaixin_HT50019_A_HSD_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hansen_HS5063FWNC39_00_CPT_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_suxian_S_40_08700A_mcu_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_yixinda_Y82635_mcu_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_aizuoertai_MS_A40230N00EO_CTC_mcu18_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_hongsheng_HS4501118A_CMI_mcu18_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_nisin_NST397WV2312ANK_V1_CMI_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_huanyutong_HY103970683128A99_HSD_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_suxian_S4014400B_BOE_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_huayu_UD0087KP040_IVO_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_nisin_LN8T397WV0249_CMI_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_haoshitong_HW397130_HSD_dsi_2_cmd_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_apex_A40305N00VO_IVO_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_apex_A45147N50HN_HSD_dsi_2_cmd_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_wanchanglong_W501298AAA_BOE_dsi_2_cmd_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_fair_FTM44660B_BOE_dsi_2_cmd_fwvga_lcm_drv;
+extern LCM_DRIVER otm8009a_jingtai_JTD040347B1_BOE_dsi_2_wvga_lcm_drv;
+extern LCM_DRIVER otm8009a_haoshitong_HF4588_BOE_mcu18_fwvga_lcm_drv;
+//!!!!!!!!!!!!
+extern LCM_DRIVER hx8369_6575_lcm_drv;
+extern LCM_DRIVER hx8363_6575_dsi_lcm_drv;
+extern LCM_DRIVER hx8363_6575_dsi_hvga_lcm_drv;
+extern LCM_DRIVER bm8578_lcm_drv;
+extern LCM_DRIVER nt35582_mcu_lcm_drv;
+extern LCM_DRIVER nt35582_mcu_6575_lcm_drv;
+extern LCM_DRIVER nt35582_rgb_6575_lcm_drv;
+extern LCM_DRIVER hx8357b_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_6575_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_6575_hvga_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_6575_qvga_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_vdo_lcm_drv;
+extern LCM_DRIVER hx8369b_dsi_vdo_lcm_drv;
+extern LCM_DRIVER hx8369_hvga_lcm_drv;
+extern LCM_DRIVER ili9481_lcm_drv;
+extern LCM_DRIVER nt35582_lcm_drv;
+extern LCM_DRIVER s6d0170_lcm_drv;
+extern LCM_DRIVER spfd5461a_lcm_drv;
+extern LCM_DRIVER ta7601_lcm_drv;
+extern LCM_DRIVER tft1p3037_lcm_drv;
+extern LCM_DRIVER ha5266_lcm_drv;
+extern LCM_DRIVER hsd070idw1_lcm_drv;
+extern LCM_DRIVER lg4571_lcm_drv;
+extern LCM_DRIVER lvds_wsvga_lcm_drv;
+extern LCM_DRIVER lvds_wsvga_ti_lcm_drv;
+extern LCM_DRIVER lvds_wsvga_ti_n_lcm_drv;
+extern LCM_DRIVER nt35565_3d_lcm_drv;
+extern LCM_DRIVER tm070ddh03_lcm_drv;
+extern LCM_DRIVER r61408_lcm_drv;
+extern LCM_DRIVER nt35510_lcm_drv;
+extern LCM_DRIVER nt35510_dpi_lcm_drv;
+extern LCM_DRIVER nt35510_hvga_lcm_drv;
+extern LCM_DRIVER nt35510_qvga_lcm_drv;
+extern LCM_DRIVER nt35510_6517_lcm_drv;
+extern LCM_DRIVER nt35510_dsi_lcm_drv;
+extern LCM_DRIVER lt4015w_dsi_lcm_drv;
+extern LCM_DRIVER r63303_idisplay_lcm_drv;
+extern LCM_DRIVER hj080ia_lcm_drv;
+
+#if defined(GN_SSD2825_SMD_S6E8AA)
+extern LCM_DRIVER gn_ssd2825_smd_s6e8aa;
+#endif
+extern LCM_DRIVER hx8369_dsi_bld_lcm_drv;
+extern LCM_DRIVER hx8369_dsi_tm_lcm_drv;
+extern LCM_DRIVER otm8018b_dsi_vdo_lcm_drv;	 
+extern LCM_DRIVER nt35512_dsi_vdo_lcm_drv;
+extern LCM_DRIVER hx8379a_dsi_vdo_lcm_drv;
+extern LCM_DRIVER hx8379a_dsi_vdo_bidirectional_lcm_drv ;
+extern LCM_DRIVER hx8379a_dsi_vdo_tcl_bidrectional_lcm_drv;
+extern LCM_DRIVER hx8379a_dsi_vdo_9070_lcm_drv;
+extern LCM_DRIVER otm8009a_yashi_lcm_drv;
+extern LCM_DRIVER s90446_wvga_dsi_cmd_otm8009a_lcm_drv;
+LCM_DRIVER* lcm_driver_list[] = 
+{ 
+#if defined(OTM8009A_DSI)
+	&otm8009a_dsi_lcm_drv,
+#endif
+
+#if defined(GT397_OTM8009A)
+	&gt397_otm8009a_lcm_drv,
+#endif
+
+#if defined(OTM8009A_YASHI)
+	&otm8009a_yashi_lcm_drv,
+#endif
+
+#if defined(S90446_WVGA_DSI_CMD_OTM8009A)
+	&s90446_wvga_dsi_cmd_otm8009a_lcm_drv,
+#endif
+
+//!!!!!!!!!!!
+
+#if defined(OTM8009A_AIZHUOERTAI_A4019N50Q0_CTC_MCU18_WVGA)
+	&otm8009a_aizhuoertai_a4019n50q0_mcu_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HANSEN_HS4534FWNC23_00_DSI_FWVGA)
+	&otm8009a_hansen_hs4534fwnc23_00_dsi_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HONGSHENG_HS4501071A_CMI_DSI_2_FWVGA)
+	&otm8009a_hongsheng_hs4501071a_dsi_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HONGTAO_HT45009A_V0_CPT_MCU18_FWVGA)
+	&otm8009a_hongtao_HT45009A_V0_CPT_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_NISIN_NSNST397WV3923ANP_CMI_MCU24_WVGA)
+	&otm8009a_nisin_NSNST397WV3923ANP_CMI_mcu24_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HONGDA_HDKM0493_CMI_DSI_2_FWVGA)
+	&otm8009a_hongda_HDKM0493_CMI_dsi_2_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HENTONGKAIXIN_HT50019_A_HSD_MCU18_FWVGA)
+	&otm8009a_hentongkaixin_HT50019_A_HSD_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HANSEN_HS5063FWNC39_00_CPT_MCU18_FWVGA)
+	&otm8009a_hansen_HS5063FWNC39_00_CPT_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_SUXIAN_S_40_08700A_MCU_WVGA)
+	&otm8009a_suxian_S_40_08700A_mcu_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_YIXINDA_Y82635_MCU_WVGA)
+	&otm8009a_yixinda_Y82635_mcu_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_AIZUOERTAI_MS_A40230N00EO_CTC_MCU18_WVGA)
+	&otm8009a_aizuoertai_MS_A40230N00EO_CTC_mcu18_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HONGSHENG_HS4501118A_CMI_MCU18_FWVGA)
+	&otm8009a_hongsheng_HS4501118A_CMI_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_NISIN_NST397WV2312ANK_V1_CMI_DSI_2_WVGA)
+	&otm8009a_nisin_NST397WV2312ANK_V1_CMI_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HUANYUTONG_HY103970683128A99_HSD_DSI_2_WVGA)
+	&otm8009a_huanyutong_HY103970683128A99_HSD_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_SUXIAN_S4014400B_BOE_DSI_2_WVGA)
+	&otm8009a_suxian_S4014400B_BOE_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HUAYU_UD0087KP040_IVO_DSI_2_WVGA)
+	&otm8009a_huayu_UD0087KP040_IVO_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_NISIN_LN8T397WV0249_CMI_DSI_2_WVGA)
+	&otm8009a_nisin_LN8T397WV0249_CMI_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HAOSHITONG_HW397130_HSD_DSI_2_CMD_WVGA)
+	&otm8009a_haoshitong_HW397130_HSD_dsi_2_cmd_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_APEX_A40305N00VO_IVO_DSI_2_WVGA)
+	&otm8009a_apex_A40305N00VO_IVO_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_APEX_A45147N50HN_HSD_DSI_2_CMD_FWVGA)
+	&otm8009a_apex_A45147N50HN_HSD_dsi_2_cmd_fwvga_lcm_drv,
+#endif
+#if defined(OTM8009A_WANCHANGLONG_W501298AAA_BOE_DSI_2_CMD_FWVGA)
+	&otm8009a_wanchanglong_W501298AAA_BOE_dsi_2_cmd_fwvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_FAIR_FTM44660B_BOE_DSI_2_CMD_FWVGA)
+	&otm8009a_fair_FTM44660B_BOE_dsi_2_cmd_fwvga_lcm_drv,
+#endif	
+
+#if defined(OTM8009A_JINGTAI_JTD040347B1_BOE_DSI_2_WVGA)
+	&otm8009a_jingtai_JTD040347B1_BOE_dsi_2_wvga_lcm_drv,
+#endif
+
+#if defined(OTM8009A_HAOSHITONG_HF4588_BOE_MCU18_FWVGA)
+	&otm8009a_haoshitong_HF4588_BOE_mcu18_fwvga_lcm_drv,
+#endif
+
+//!!!!!!!!!!!!
+
+#if defined(LT4015W_DSI)
+	&lt4015w_dsi_lcm_drv,
+#endif
+
+#if defined(LT5002F_DSI)
+	&lt5002f_dsi_lcm_drv,
+#endif
+
+#if defined(HX8379A_DSI_VDO)
+	&hx8379a_dsi_vdo_lcm_drv,
+#endif
+
+#if defined(HX8379A_DSI_VDO_9070)
+	&hx8379a_dsi_vdo_9070_lcm_drv,
+#endif
+
+#if defined(HX8379A_DSI_VDO_TCL_BIDRECTIONAL)
+	&hx8379a_dsi_vdo_tcl_bidrectional_lcm_drv,
+#endif
+
+#if defined(HX8379A_DSI_VDO_BIDIRECTIONAL)
+	&hx8379a_dsi_vdo_bidirectional_lcm_drv,
+#endif
+
+#if defined(HX8369A)
+	&hx8369a_lcm_drv,
+#endif
+
+#if defined(HX8369_6575)
+	&hx8369_6575_lcm_drv,
+#endif
+
+#if defined(BM8578)
+	&bm8578_lcm_drv,
+#endif
+
+#if defined(NT35582_MCU)
+	&nt35582_mcu_lcm_drv,
+#endif
+
+#if defined(NT35582_MCU_6575)
+	&nt35582_mcu_6575_lcm_drv,
+#endif
+
+#if defined(NT35582_RGB_6575)
+	&nt35582_rgb_6575_lcm_drv,
+#endif
+
+#if defined(HX8357B)
+	&hx8357b_lcm_drv,
+#endif
+
+#if defined(R61408)
+	&r61408_lcm_drv,
+#endif
+
+#if defined(HX8369_DSI_VDO)
+	&hx8369_dsi_vdo_lcm_drv,
+#endif
+
+#if defined(HX8369_DSI)
+	&hx8369_dsi_lcm_drv,
+#endif
+
+#if defined(HX8369_6575_DSI)
+	&hx8369_dsi_6575_lcm_drv,
+#endif
+
+#if defined(HX8369_6575_DSI_NFC_ZTE)
+	&hx8369_dsi_6575_lcm_drv,
+#endif
+
+#if defined(HX8369_6575_DSI_HVGA)
+	&hx8369_dsi_6575_hvga_lcm_drv,
+#endif
+
+#if defined(HX8369_6575_DSI_QVGA)
+	&hx8369_dsi_6575_qvga_lcm_drv,
+#endif
+
+#if defined(HX8369_HVGA)
+	&hx8369_hvga_lcm_drv,
+#endif
+
+#if defined(NT35510)
+	&nt35510_lcm_drv,
+#endif
+
+#if defined(NT35510_RGB_6575) 
+	&nt35510_dpi_lcm_drv,
+#endif	
+	
+
+#if defined(NT35510_HVGA)
+	&nt35510_hvga_lcm_drv,
+#endif
+
+#if defined(NT35510_QVGA)
+	&nt35510_qvga_lcm_drv,
+#endif
+
+#if defined(NT35510_6517)
+	&nt35510_6517_lcm_drv,
+#endif
+
+#if defined(ILI9481)
+	&ili9481_lcm_drv,
+#endif
+
+#if defined(NT35582)
+	&nt35582_lcm_drv,
+#endif
+
+#if defined(S6D0170)
+	&s6d0170_lcm_drv,
+#endif
+
+#if defined(SPFD5461A)
+	&spfd5461a_lcm_drv,
+#endif
+
+#if defined(TA7601)
+	&ta7601_lcm_drv,
+#endif
+
+#if defined(TFT1P3037)
+	&tft1p3037_lcm_drv,
+#endif
+
+#if defined(HA5266)
+	&ha5266_lcm_drv,
+#endif
+
+#if defined(HSD070IDW1)
+	&hsd070idw1_lcm_drv,
+#endif
+
+#if defined(HX8363_6575_DSI)
+	&hx8363_6575_dsi_lcm_drv,
+#endif
+
+#if defined(HX8363_6575_DSI_HVGA)
+	&hx8363_6575_dsi_hvga_lcm_drv,
+#endif
+
+#if defined(LG4571)
+	&lg4571_lcm_drv,
+#endif
+
+#if defined(LVDS_WSVGA)
+	&lvds_wsvga_lcm_drv,
+#endif
+
+#if defined(LVDS_WSVGA_TI)
+	&lvds_wsvga_ti_lcm_drv,
+#endif
+
+#if defined(LVDS_WSVGA_TI_N)
+	&lvds_wsvga_ti_n_lcm_drv,
+#endif
+
+#if defined(NT35565_3D)
+	&nt35565_3d_lcm_drv,
+#endif
+
+#if defined(TM070DDH03)
+	&tm070ddh03_lcm_drv,
+#endif
+
+#if defined(NT35510_DSI)
+	&nt35510_dsi_lcm_drv,
+#endif
+
+#if defined(OTM8009A_FWVGA_DSI_CMD_TIANMA)
+	&otm8009a_fwvga_dsi_cmd_tianma_lcm_drv,
+#endif
+
+#if defined(OTM8009A_FWVGA_DSI_VDO_TIANMA)
+	&otm8009a_fwvga_dsi_vdo_tianma_lcm_drv,
+#endif
+
+#if (defined(OTM8009A_HSD53_JTAI) || defined(OTM8009A_HSD53_ZES))
+	&otm8009a_dsi_lcm_drv,
+#endif
+
+#if defined(OTM8009_6517_DSI)
+        &otm8009_dsi_6517_lcm_drv,
+#endif
+
+#if defined(OTM8009A)
+        &otm8009a_lcm_drv,
+#endif
+
+#if defined(HX8369A_WVGA_DSI_CMD)
+	&hx8369a_wvga_dsi_cmd_drv,
+#endif
+
+#if defined(HX8369A_WVGA_DSI_VDO)
+	&hx8369a_wvga_dsi_vdo_drv,
+#endif
+
+#if defined(HX8369A)
+	&hx8369a_lcm_drv,
+#endif
+
+#if defined(HX8369A_HUAXIAN_HX451023FP_BOE_DSI_2_FWVGA)
+	&hx8369a_huaxian_HX451023FP_BOE_dsi_2_fwvga_lcm_drv,
+#endif
+
+#if defined(HX8369A_NISIN_NST446FW2302ANK_TM_DSI_2_FWVGA)
+	&hx8369a_nisin_NST446FW2302ANK_TM_dsi_2_fwvga_lcm_drv,
+#endif
+
+#if defined(HX8369A_HAIFEI_HF_14539_2059_A0_CMI_MCU18_FWVGA)
+	&hx8369a_haifei_HF_14539_2059_A0_CMI_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(HX8369A_HENGSHENG_HS045IK_01B_TM_MCU18_FWVGA)
+	&hx8369a_hengsheng_HS045IK_01B_TM_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(HX8369A_HENGSHENG_HS040IH_01E_TM_MCU18_WVGA)
+	&hx8369a_hengsheng_HS040IH_01E_TM_mcu18_wvga_lcm_drv,
+#endif
+
+#if defined(HX8369A_HUARUI_MS_HX450939FP_BOE_MCU18_FWVGA)
+	&hx8369a_HUARUI_MS_HX450939FP_BOE_mcu18_fwvga_lcm_drv,
+#endif
+
+#if defined(R63303_IDISPLAY)
+	&r63303_idisplay_lcm_drv,
+#endif
+
+#if defined(HX8369B_DSI_VDO)
+	&hx8369b_dsi_vdo_lcm_drv,
+#endif
+
+#if defined(GN_SSD2825_SMD_S6E8AA)
+	&gn_ssd2825_smd_s6e8aa,
+#endif
+#if defined(HX8369_TM_DSI)
+	&hx8369_dsi_tm_lcm_drv,
+#endif
+
+#if defined(HX8369_BLD_DSI)
+	&hx8369_dsi_bld_lcm_drv,
+#endif
+
+#if defined(HJ080IA)
+	&hj080ia_lcm_drv,
+#endif
+#if defined(OTM8018B_DSI_VDO)	
+	&otm8018b_dsi_vdo_lcm_drv, 
+#endif
+
+#if defined(NT35512_DSI_VDO)
+	&nt35512_dsi_vdo_lcm_drv, 
+#endif
+};
+
+#define LCM_COMPILE_ASSERT(condition) LCM_COMPILE_ASSERT_X(condition, __LINE__)
+#define LCM_COMPILE_ASSERT_X(condition, line) LCM_COMPILE_ASSERT_XX(condition, line)
+#define LCM_COMPILE_ASSERT_XX(condition, line) char assertion_failed_at_line_##line[(condition)?1:-1]
+
+unsigned int lcm_count = sizeof(lcm_driver_list)/sizeof(LCM_DRIVER*);
+LCM_COMPILE_ASSERT(0 != sizeof(lcm_driver_list)/sizeof(LCM_DRIVER*));
